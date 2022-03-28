f62fb0b036a67d6a50c42f497148b2eb53626abf
Thon Ly <thonly@heartbank.ceo>
2022-03-28 11:32:45 -0700

    created landing pages

---
+++ frontend/game.js
@@ -0,0 +1,7 @@
+console.log("hi");
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SZZBTK3QFK');
---
+++ index.js
@@ -0,0 +1,12 @@
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SZZBTK3QFK');
+
+/*
+
+ideas
+1. user can chooses which cards to be tested
+
+*/

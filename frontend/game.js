c27bbff7452cef01eb1d4cfcc08f0bb097dc4611
Thon Ly <thonly@heartbank.ceo>
2022-04-06 22:09:29 -0700

    css transitions and animations

--- frontend/game.js
+++ frontend/game.js
@@ -1,4 +1,15 @@
-console.log("hi");
+function flip(direction) {
+  const main = document.querySelector("main")
+  main.classList.toggle("flip", direction)
+}
+
+function slide(direction) {
+  const main = document.querySelector("main")
+  main.style.animation = "none";
+  main.offsetHeight; /* trigger reflow */
+  main.style.animation = null; 
+  main.style.animationName = direction ? "slideLeft" : "slideRight";
+}
 
 window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
